88673453695b8ee6b60311211a4e41c39238b115
Brittany <BrittanyKohler@gmail.com>
2015-12-08 12:16:25 -0800

    Adding CRUD methods to product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  
+
   resources :products do
     resources :reviews, except: [:index]
   end
 
   resources :merchants do
-    resources :products, only: [:show]
+    resources :products, only: [:index]
   end
 
   resources :categories do
-    resources :products, only: [:show]
+    resources :products, only: [:index]
   end
 
   resources :order do
