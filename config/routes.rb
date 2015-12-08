6b7073104b4702e1f373c41ce75d5263e662166c
emgord <emgord@gmail.com>
2015-12-08 11:34:08 -0800

    created rspec test for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+  
   resources :products do
     resources :reviews, except: [:index]
   end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  describe "GET 'index'" do
+  it "is successful" do
+    get :index
+    expect(response.status).to eq 200
+  end
+end
+
 end
