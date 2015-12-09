aa60f8ee1dcb2e15c68c4c846fec6163cd97ab88
Brittany <BrittanyKohler@gmail.com>
2015-12-09 11:02:57 -0800

    Fixing missing end in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_order
-  if !session[:order_id].nil?
-    Order.find(session[:order_id])
-  else
-    Order.new
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
   end
 end
