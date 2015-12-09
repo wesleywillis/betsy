3d8f8e0336ce8abca1e0aedfbcd430c1e8fabd49
emgord <emgord@gmail.com>
2015-12-09 15:54:03 -0800

    added specs and validations for merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ActiveRecord::Base
   has_many :products
   has_secure_password
+
+  validates :user_name, presence: true, uniqueness: true, length: { maximum: 25 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
+
 end
