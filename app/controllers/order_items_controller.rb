9b4e311526249abeacda8fe9d922d2a15625edd3
Brittany <BrittanyKohler@gmail.com>
2015-12-09 15:50:44 -0800

    Updating styling for cart page

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,9 @@
 class OrderItemsController < ApplicationController
   def create
     @order = current_order
+    sdfsd
+    # if @order.order_items.product_id
+    end
     @order_item = @order.order_items.create(order_item_params)
     session[:order_id] = @order.id
     redirect_to cart_path
@@ -13,6 +16,17 @@ class OrderItemsController < ApplicationController
       Order.create
     end
   end
+
+  # def update
+  #   if order_item_params[:quantity].to_i <= @order_item.product.stock.to_i
+  #     @order_item.update(order_item_params)
+  #     @order_items = @order.order_items
+  #     redirect_to order_path(@order)
+  #   else
+  #     flash[:error] = "Unfortunately we don't have #{order_item_params[:quantity].to_i} #{@order_item.product.name}, only #{@order_item.product.stock.to_i} available"
+  #     redirect_to cart_path
+  #   end
+  # end
 
   private
 
