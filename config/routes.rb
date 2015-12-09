17e33bc7e1917cb383a683898f4d3a6f37d1dec9
Rebecca Tolmach <rmtolmach@gmail.com>
2015-12-09 11:02:01 -0800

    added routes for brittany

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-<<<<<<< HEAD
   resource :cart, only: [:show]
 
-  resources :products do
-=======
   resources :products, except: [:new, :edit] do
->>>>>>> 7ee8c1cc54815e1d7b2764944c92c41caab3b82a
     resources :reviews, except: [:index]
   end
 
