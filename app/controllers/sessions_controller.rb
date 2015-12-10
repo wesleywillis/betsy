7f3c2c4f6b54981f59b043910a1ace00894f3cde
Rebecca Tolmach <rmtolmach@gmail.com>
2015-12-10 15:43:24 -0800

    created view for new session and started controller methods for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  # require login needs to be created in the ApplicationController to create flash message
+  # skip_before_action :require_login, only: [:new, :create]
+
+  def new
+
+  end
+
+  def create
+  # session_data is created in our form_for in sessions view page.
+  end
 end
