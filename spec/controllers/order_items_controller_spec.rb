c5d9349c98a4e00612208a59fa3e8b1e94a38dec
Brittany <BrittanyKohler@gmail.com>
2015-12-10 12:19:59 -0800

    Wrote first rspec test for order_items_controller

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe OrderItemsController, type: :controller do
+  describe "POST 'create'" do
+    let (:params) do
+      {
+        order_item:{
+          quantity: 1, product_id: 2, order_id: 1
+        }
+      }
+    end
+
+    it "adds an order item to the order" do
+
+    end
+
+    it "updates the quantity of an order item when the order item is already in cart" do
+
+    end
+
+    it "redirects to cart once order item is added" do
+      post :create, params
+      expect(subject).to redirect_to cart_path
+    end
+  end
 
 end
