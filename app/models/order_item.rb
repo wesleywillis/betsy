785e0f7c8c5e5a0d2d0cfc794f04b6a00fd46c6f
emgord <emgord@gmail.com>
2015-12-10 11:11:52 -0800

    added spec and valdiations for order items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  validates :product_id, presence: true
+  validates :order_id, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
