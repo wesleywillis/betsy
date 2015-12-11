47e99a3a41d32b67730c54768ff85318432ac089
emgord <emgord@gmail.com>
2015-12-11 12:22:53 -0800

    login button shows if not logged in, logout button shows if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @categories = Category.all
-    if current_user.nil?
-      @user_name = "Guest"
-    else
-      @user_name = current_user.user_name
-    end
     @featured_products = Product.all.shuffle.last(6)
     @merchants = Merchant.all
   end
