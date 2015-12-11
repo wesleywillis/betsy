a19e4faa3cb3db157b16a602e978d0f479b5861a
emgord <emgord@gmail.com>
2015-12-11 09:58:49 -0800

    added guest user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
 
   def index
     @categories = Category.all
-    @user_name = current_user.user_name
+    if current_user.user_name.nil?
+      @user_name = "Guest"
+    else
+      @user_name = current_user.user_name
+    end
     @featured_products = Product.all.shuffle.last(6)
     @merchants = Merchant.all
   end
