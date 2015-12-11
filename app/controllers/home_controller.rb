d6a8da3ddc3e2d941d1ea3b816f7bf4a39c7443a
emgord <emgord@gmail.com>
2015-12-10 22:11:58 -0800

    added name to home page if logged in, and only shows delete and edit button on items that merchant owns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @featured_products = Product.all.shuffle.last(10)
+    @user_name = current_user.user_name
   end
 end
