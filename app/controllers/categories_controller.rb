f762b0904a74d8ed8d0036c988a53ce6a0f318a4
emgord <emgord@gmail.com>
2015-12-15 12:49:31 -0800

    merchant can add new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,25 @@
 class CategoriesController < ApplicationController
+
+  before_action :require_user, only: [:new, :create]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      redirect_to merchant_path(@current_user)
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
