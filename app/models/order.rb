0c544e27f37a6f27e391cce541284d8ca7cc0af2
Brittany <BrittanyKohler@gmail.com>
2015-12-14 16:30:57 -0800

    Adding numericality validations for checkout form

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,13 @@ class Order < ActiveRecord::Base
   validates :customer_email, presence: true, on: :update, if: :paid?
   validates :street_address, presence: true, on: :update, if: :paid?
   validates :city, presence: true, on: :update, if: :paid?
-  validates :zip_code, presence: true, on: :update, if: :paid?
+  validates :zip_code, presence: true, length: {minimum: 5 }, on: :update, if: :paid?
   validates :name_on_card, presence: true, on: :update, if: :paid?
-  validates :card_number, presence: true, on: :update, if: :paid?
-  validates :customer_card_exp_month, presence: true, on: :update, if: :paid?
-  validates :billing_zip_code, presence: true, on: :update, if: :paid?
-  validates :security_code, presence: true, on: :update, if: :paid?
+  validates :card_number, presence: true, length: {minimum: 16 }, on: :update, if: :paid?
+  validates :customer_card_exp_month, presence: true, numericality: { greater_than: 0 }, on: :update, if: :paid?
+validates :customer_card_exp_year, presence: true, numericality: { greater_than: 0 }, on: :update, if: :paid?
+  validates :billing_zip_code, presence: true, length: {minimum: 5 }, on: :update, if: :paid?
+  validates :security_code, presence: true, length: {minimum: 3 }, on: :update, if: :paid?
 
   def paid?
     status == "paid"
