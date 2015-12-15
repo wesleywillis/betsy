fe5561e239db36ef5acf34f77896255aa63c4fbe
Wesley Willis <haileyw@gmail.com>
2015-12-15 11:40:22 -0800

    fix bugs caused by new migration on order show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151214184208) do
+ActiveRecord::Schema.define(version: 20151215003728) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
