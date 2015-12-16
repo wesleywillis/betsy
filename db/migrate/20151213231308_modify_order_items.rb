ab212bb2a94890976a926478aeb4ed752e52c16b
Rebecca Tolmach <rmtolmach@gmail.com>
2015-12-16 14:08:45 -0800

    commented out migration for order items

--- db/migrate/20151213231308_modify_order_items.rb
+++ db/migrate/20151213231308_modify_order_items.rb
@@ -1,6 +1,6 @@
 class ModifyOrderItems < ActiveRecord::Migration
   def change
-    remove_column :order_items, :status, :string
+    # remove_column :order_items, :status, :string
     add_column :order_items, :status, :boolean
   end
 end
