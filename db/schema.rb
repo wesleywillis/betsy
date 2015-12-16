fb3f490aa563ef7e4d80ea97cf1153ce02c94864
Rebecca Tolmach <rmtolmach@gmail.com>
2015-12-16 10:26:13 -0800

    added default value for retire column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215003728) do
+ActiveRecord::Schema.define(version: 20151216181539) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -69,9 +69,9 @@ ActiveRecord::Schema.define(version: 20151215003728) do
     t.string   "description"
     t.string   "photo_url"
     t.integer  "inventory"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "retire"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "retire",      default: false
   end
 
   create_table "reviews", force: :cascade do |t|
