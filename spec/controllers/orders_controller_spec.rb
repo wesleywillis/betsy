d8fa52955e6f1b748fcb6090ca1a720a1d369921
Brittany <BrittanyKohler@gmail.com>
2015-12-17 15:30:55 -0800

    Updating show method test for orders controller

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe OrdersController, type: :controller do
 
       it "provides the gramatically-correct error message if the item quantity is greater than the product inventory" do
         order = Order.create(status: "pending")
-        p1 = Product.create(name: "test thing", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 1)
+        p1 = Product.create(name: "test thing2", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 1)
         @order_item = OrderItem.create(quantity: 1, product_id: p1.id , order_id: order.id)
         p1.inventory = 0
         p1.save
@@ -31,7 +31,7 @@ RSpec.describe OrdersController, type: :controller do
 
       it "provides the gramatically-correct error message if the item quantity is greater than the product inventory" do
         order = Order.create(status: "pending")
-        p1 = Product.create(name: "test thing", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
+        p1 = Product.create(name: "test thing3", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
         @order_item = OrderItem.create(quantity: 2, product_id: p1.id , order_id: order.id)
         p1.inventory = 1
         p1.save
@@ -42,7 +42,7 @@ RSpec.describe OrdersController, type: :controller do
 
       it "provides the gramatically-correct error message if the item quantity is greater than the product inventory" do
         order = Order.create(status: "pending")
-        p1 = Product.create(name: "test thing", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
+        p1 = Product.create(name: "test thing4", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
         @order_item = OrderItem.create(quantity: 3, product_id: p1.id , order_id: order.id)
         p1.inventory = 2
         p1.save
@@ -72,7 +72,7 @@ RSpec.describe OrdersController, type: :controller do
 
     before (:each) do
       order = Order.create(status: "pending")
-      Product.create(name: "testy", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
+      Product.create(name: "magic stuff", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
       @order_item = OrderItem.create(quantity: 1, product_id: 1, order_id: order.id)
       session[:order_id] = order.id
     end
@@ -101,35 +101,17 @@ RSpec.describe OrdersController, type: :controller do
   end
 
   describe "Logged in functions" do
-    let (:merchant) do
-      Merchant.create(user_name: "Hailey", email: "spiderlover222@hogwarts.com", password: "123", password_confirmation: "123")
-    end
-
-    let (:product) do
-      Product.create(name: "test thing", price: 10, merchant_id: 1, description: "hi", photo_url: "www.google.com", inventory: 4)
-    end
-
-    let (:order_item) do
-      OrderItem.create(quantity: 1, product_id: product.id, order_id: 1)
-    end
-
-    let (:order) do
-      Order.create(status: "pending", order_time: Time.now, customer_name: "Minerva McGonagall", customer_email: "miverva@hogwarts.com", street_address: "Hogwarts Castle", zip_code: 12345, state: "Washington", city: "Hogwarts", card_number: 1234, customer_card_exp_month: 10, customer_card_exp_year: 2018, security_code: 123, name_on_card: "Minerva McGonagall" )
-    end
-
     before (:each) do
+      merchant = Merchant.create(user_name: "Hailey", email: "spiderlover222@hogwarts.com", password: "123", password_confirmation: "123")
+      product = Product.create(name: "test thing19", price: 10, merchant_id: merchant.id, description: "hi", photo_url: "www.google.com", inventory: 4)
+      @order = Order.create(status: "pending", order_time: Time.now, customer_name: "Minerva McGonagall", customer_email: "miverva@hogwarts.com", street_address: "Hogwarts Castle", zip_code: 12345, state: "Washington", city: "Hogwarts", card_number: 1234, customer_card_exp_month: 10, customer_card_exp_year: 2018, security_code: 123, name_on_card: "Minerva McGonagall" )
+      order_item = OrderItem.create(quantity: 1, product_id: product.id, order_id: @order.id)
       session[:merchant_id] = merchant.id
     end
 
     describe "GET #show" do
-      it "responds successfully with an HTTP 200 status code" do
-        get :show, id: order.id
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
-      end
-
       it "renders the show template if there are items in the order that belong to the merchant" do
-        get :show, id: order.id
+        get :show, id: @order.id
         expect(subject).to render_template :show
       end
     end
