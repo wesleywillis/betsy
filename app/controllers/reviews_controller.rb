32fbb503386f912c2817156d11089d4f5ac479fc
Wesley Willis <haileyw@gmail.com>
2015-12-17 14:15:48 -0800

    add redirect and error to prevent merchant from leaving review on own product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,17 +6,22 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.create(review_params)
     @review.product_id = params[:product_id]
-    if @review.save
-      redirect_to product_path(@review.product_id)
+
+    if @current_user.products.include?!(@review.product_id) || @current_user == nil
+      if @review.save
+        redirect_to product_path(@review.product)
+      else
+        render "new"
+      end
     else
-      render "new"
+      redirect_to product_path(@review.product_id)
     end
+    flash[:error] = "You aren't allowed to review your own products.  Stop being Slytherin."
   end
 
 private
 
-def review_params
-  params.require(:review).permit(:rating, :description)
-end
-
+  def review_params
+    params.require(:review).permit(:rating, :description)
+  end
 end
